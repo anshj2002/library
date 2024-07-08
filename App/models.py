a0c1d7535bca77ba376a3c5d2713231fe1de409b
anshj <anshjain88825@gmail.com>
2024-07-08 21:14:17 +0530

    hmm7

--- App/models.py
+++ App/models.py
@@ -5,9 +5,9 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
-    role = db.Column(db.String(10), nullable=False)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    password_hash = db.Column(db.String(420), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
